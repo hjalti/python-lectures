5a1be8bc5056eacc0b06361b0377e44366f610d3
Hjalti Magnusson <hjaltmann@gmail.com>
2017-11-29 02:06:34 +0000

    We should delete this crap before shipping.

---
+++ day_03/data/countries.py
@@ -0,0 +1,234 @@
+countries =  '''Afghanistan 	Asia 	Southern Asia 	34,656,032 
+ Albania 	Europe 	Southern Europe 	2,926,348 
+ Algeria 	Africa 	Northern Africa 	40,606,052 
+ American Samoa 	Oceania 	Polynesia 	55,599 
+ Andorra 	Europe 	Southern Europe 	77,281 
+ Angola 	Africa 	Middle Africa 	28,813,463 
+ Anguilla 	Americas 	Caribbean 	14,764 
+ Antigua and Barbuda 	Americas 	Caribbean 	100,963 
+ Argentina 	Americas 	South America 	43,847,430 
+ Armenia 	Asia 	Western Asia 	2,924,816 
+ Aruba 	Americas 	Caribbean 	104,822 
+ Australia[f] 	Oceania 	Australia and New Zealand 	24,125,848 
+ Austria 	Europe 	Western Europe 	8,712,137 
+ Azerbaijan[h] 	Asia 	Western Asia 	9,725,376 
+ Bahamas 	Americas 	Caribbean 	391,232 
+ Bahrain 	Asia 	Western Asia 	1,425,171 
+ Bangladesh 	Asia 	Southern Asia 	162,951,560 
+ Barbados 	Americas 	Caribbean 	284,996 
+ Belarus 	Europe 	Eastern Europe 	9,480,042 
+ Belgium 	Europe 	Western Europe 	11,358,379 
+ Belize 	Americas 	Central America 	366,954 
+ Benin 	Africa 	Western Africa 	10,872,298 
+ Bermuda 	Americas 	Northern America 	61,666 
+ Bhutan 	Asia 	Southern Asia 	797,765 
+ Bolivia 	Americas 	South America 	10,887,882 
+ Bosnia and Herzegovina 	Europe 	Southern Europe 	3,516,816 
+ Botswana 	Africa 	Southern Africa 	2,250,260 
+ Brazil 	Americas 	South America 	207,652,865 
+ British Virgin Islands 	Americas 	Caribbean 	30,661 
+ Brunei 	Asia 	South-Eastern Asia 	423,196 
+ Bulgaria 	Europe 	Eastern Europe 	7,131,494 
+ Burkina Faso 	Africa 	Western Africa 	18,646,433 
+ Burundi 	Africa 	Eastern Africa 	10,524,117 
+ Cambodia 	Asia 	South-Eastern Asia 	15,762,370 
+ Cameroon 	Africa 	Middle Africa 	23,439,189 
+ Canada 	Americas 	Northern America 	36,289,822 
+ Cape Verde 	Africa 	Western Africa 	539,560 
+ Caribbean Netherlands[r] 	Americas 	Caribbean 	25,019 
+ Cayman Islands 	Americas 	Caribbean 	60,765 
+ Central African Republic 	Africa 	Middle Africa 	4,594,621 
+ Chad 	Africa 	Middle Africa 	14,452,543 
+ Chile 	Americas 	South America 	17,909,754 
+ China[a] 	Asia 	Eastern Asia 	1,403,500,365 
+ Colombia 	Americas 	South America 	48,653,419 
+ Comoros 	Africa 	Eastern Africa 	795,601 
+ Congo 	Africa 	Middle Africa 	5,125,821 
+ Cook Islands 	Oceania 	Polynesia 	17,379 
+ Costa Rica 	Americas 	Central America 	4,857,274 
+ Croatia 	Europe 	Southern Europe 	4,213,265 
+ Cuba 	Americas 	Caribbean 	11,475,982 
+ Curaçao 	Americas 	Caribbean 	159,371 
+ Cyprus[p] 	Europe 	Europe 	1,170,125 
+ Czech Republic 	Europe 	Eastern Europe 	10,610,947 
+ Democratic Republic of the Congo 	Africa 	Middle Africa 	78,736,153 
+ Denmark 	Europe 	Northern Europe 	5,711,870 
+ Djibouti 	Africa 	Eastern Africa 	942,333 
+ Dominica 	Americas 	Caribbean 	73,543 
+ Dominican Republic 	Americas 	Caribbean 	10,648,791 
+ East Timor 	Asia 	South-Eastern Asia 	1,268,671 
+ Ecuador 	Americas 	South America 	16,385,068 
+ Egypt 	Africa 	Northern Africa 	95,688,681 
+ El Salvador 	Americas 	Central America 	6,344,722 
+ Equatorial Guinea 	Africa 	Middle Africa 	1,221,490 
+ Eritrea 	Africa 	Eastern Africa 	4,954,645 
+ Estonia 	Europe 	Northern Europe 	1,312,442 
+ Ethiopia 	Africa 	Eastern Africa 	102,403,196 
+ Falkland Islands 	Americas 	South America 	2,910 
+ Faroe Islands 	Europe 	Northern Europe 	49,117 
+ Federated States of Micronesia 	Oceania 	Micronesia 	104,937 
+ Fiji 	Oceania 	Melanesia 	898,760 
+ Finland[j] 	Europe 	Northern Europe 	5,503,132 
+ France 	Europe 	Western Europe 	64,720,690 
+ French Guiana 	Americas 	South America 	275,713 
+ French Polynesia 	Oceania 	Polynesia 	280,208 
+ Gabon 	Africa 	Middle Africa 	1,979,786 
+ Georgia[n] 	Asia 	Western Asia 	3,925,405 
+ Germany 	Europe 	Western Europe 	81,914,672 
+ Ghana 	Africa 	Western Africa 	28,206,728 
+ Gibraltar 	Europe 	Southern Europe 	34,408 
+ Greece 	Europe 	Southern Europe 	11,183,716 
+ Greenland 	Americas 	Northern America 	56,412 
+ Grenada 	Americas 	Caribbean 	107,317 
+ Guadeloupe[q] 	Americas 	Caribbean 	449,975 
+ Guam 	Oceania 	Micronesia 	162,896 
+ Guatemala 	Americas 	Central America 	16,582,469 
+ Guernsey and  Jersey 	Europe 	Northern Europe 	164,541 
+ Guinea 	Africa 	Western Africa 	12,395,924 
+ Guinea-Bissau 	Africa 	Western Africa 	1,815,698 
+ Guyana 	Americas 	South America 	773,303 
+ Haiti 	Americas 	Caribbean 	10,847,334 
+ Honduras 	Americas 	Central America 	9,112,867 
+ Hong Kong 	Asia 	Eastern Asia 	7,302,843 
+ Hungary 	Europe 	Eastern Europe 	9,753,281 
+ Iceland 	Europe 	Northern Europe 	332,474 
+ India 	Asia 	Southern Asia 	1,324,171,354 
+ Indonesia 	Asia 	South-Eastern Asia 	261,115,456 
+ Iran 	Asia 	Southern Asia 	80,277,428 
+ Iraq 	Asia 	Western Asia 	37,202,572 
+ Ireland 	Europe 	Northern Europe 	4,726,078 
+ Isle of Man 	Europe 	Northern Europe 	83,737 
+ Israel 	Asia 	Western Asia 	8,191,828 
+ Italy 	Europe 	Southern Europe 	59,429,938 
+ Ivory Coast 	Africa 	Western Africa 	23,695,919 
+ Jamaica 	Americas 	Caribbean 	2,881,355 
+ Japan 	Asia 	Eastern Asia 	127,748,513 
+ Jordan 	Asia 	Western Asia 	9,455,802 
+ Kazakhstan 	Asia 	Central Asia 	17,987,736 
+ Kenya 	Africa 	Eastern Africa 	48,461,567 
+ Kiribati 	Oceania 	Micronesia 	114,395 
+ Kuwait 	Asia 	Western Asia 	4,052,584 
+ Kyrgyzstan 	Asia 	Central Asia 	5,955,734 
+ Laos 	Asia 	South-Eastern Asia 	6,758,353 
+ Latvia 	Europe 	Northern Europe 	1,970,530 
+ Lebanon 	Asia 	Western Asia 	6,006,668 
+ Lesotho 	Africa 	Southern Africa 	2,203,821 
+ Liberia 	Africa 	Western Africa 	4,613,823 
+ Libya 	Africa 	Northern Africa 	6,293,253 
+ Liechtenstein 	Europe 	Western Europe 	37,666 
+ Lithuania 	Europe 	Northern Europe 	2,908,249 
+ Luxembourg 	Europe 	Western Europe 	575,747 
+ Macau 	Asia 	Eastern Asia 	612,167 
+ Madagascar 	Africa 	Eastern Africa 	24,894,551 
+ Malawi 	Africa 	Eastern Africa 	18,091,575 
+ Malaysia[e] 	Asia 	South-Eastern Asia 	31,187,265 
+ Maldives 	Asia 	Southern Asia 	427,756 
+ Mali 	Africa 	Western Africa 	17,994,837 
+ Malta 	Europe 	Southern Europe 	429,362 
+ Marshall Islands 	Oceania 	Micronesia 	53,066 
+ Martinique 	Americas 	Caribbean 	385,103 
+ Mauritania 	Africa 	Western Africa 	4,301,018 
+ Mauritius[o] 	Africa 	Eastern Africa 	1,262,132 
+ Mayotte 	Africa 	Eastern Africa 	246,489 
+ Mexico 	Americas 	Central America 	127,540,423 
+ Moldova[m] 	Europe 	Eastern Europe 	4,059,608 
+ Monaco 	Europe 	Western Europe 	38,499 
+ Mongolia 	Asia 	Eastern Asia 	3,027,398 
+ Montenegro 	Europe 	Southern Europe 	628,615 
+ Montserrat 	Americas 	Caribbean 	5,152 
+ Morocco 	Africa 	Northern Africa 	35,276,786 
+ Mozambique 	Africa 	Eastern Africa 	28,829,476 
+ Myanmar 	Asia 	South-Eastern Asia 	52,885,223 
+ Namibia 	Africa 	Southern Africa 	2,479,713 
+ Nauru 	Oceania 	Micronesia 	11,347 
+   Nepal 	Asia 	Southern Asia 	28,982,771 
+ Netherlands 	Europe 	Western Europe 	16,987,330 
+ New Caledonia 	Oceania 	Melanesia 	272,677 
+ New Zealand 	Oceania 	Australia and New Zealand 	4,660,833 
+ Nicaragua 	Americas 	Central America 	6,149,928 
+ Niger 	Africa 	Western Africa 	20,672,987 
+ Nigeria 	Africa 	Western Africa 	185,989,640 
+ Niue 	Oceania 	Polynesia 	1,624 
+ North Korea 	Asia 	Eastern Asia 	25,368,620 
+ Northern Mariana Islands 	Oceania 	Micronesia 	55,023 
+ Norway[k] 	Europe 	Northern Europe 	5,254,694 
+ Oman 	Asia 	Western Asia 	4,424,762 
+ Pakistan 	Asia 	Southern Asia 	193,203,476 
+ Palau 	Oceania 	Micronesia 	21,503 
+ Palestine[l] 	Asia 	Western Asia 	4,790,705 
+ Panama 	Americas 	Central America 	4,034,119 
+ Papua New Guinea 	Oceania 	Melanesia 	8,084,991 
+ Paraguay 	Americas 	South America 	6,725,308 
+ Peru 	Americas 	South America 	31,773,839 
+ Philippines 	Asia 	South-Eastern Asia 	103,320,222 
+ Poland 	Europe 	Eastern Europe 	38,224,410 
+ Portugal 	Europe 	Southern Europe 	10,371,627 
+ Puerto Rico 	Americas 	Caribbean 	3,667,903 
+ Qatar 	Asia 	Western Asia 	2,569,804 
+ Republic of Macedonia 	Europe 	Southern Europe 	2,081,206 
+ Romania 	Europe 	Eastern Europe 	19,778,083 
+ Russia 	Europe 	Eastern Europe 	143,964,513 
+ Rwanda 	Africa 	Eastern Africa 	11,917,508 
+ Réunion 	Africa 	Eastern Africa 	869,925 
+ Saint Helena, Ascension and Tristan da Cunha 	Africa 	Western Africa 	4,035 
+ Saint Kitts and Nevis 	Americas 	Caribbean 	54,821 
+ Saint Lucia 	Americas 	Caribbean 	178,015 
+ Saint Pierre and Miquelon 	Americas 	Northern America 	6,305 
+ Saint Vincent and the Grenadines 	Americas 	Caribbean 	109,643 
+ Samoa 	Oceania 	Polynesia 	195,125 
+ San Marino 	Europe 	Southern Europe 	33,203 
+ Sao Tome and Principe 	Africa 	Middle Africa 	199,910 
+ Saudi Arabia 	Asia 	Western Asia 	32,275,687 
+ Senegal 	Africa 	Western Africa 	15,411,614 
+ Serbia[i] 	Europe 	Southern Europe 	8,820,083 
+ Seychelles 	Africa 	Eastern Africa 	94,228 
+ Sierra Leone 	Africa 	Western Africa 	7,396,190 
+ Singapore 	Asia 	South-Eastern Asia 	5,622,455 
+ Sint Maarten 	Americas 	Caribbean 	39,537 
+ Slovakia 	Europe 	Eastern Europe 	5,444,218 
+ Slovenia 	Europe 	Southern Europe 	2,077,862 
+ Solomon Islands 	Oceania 	Melanesia 	599,419 
+ Somalia 	Africa 	Eastern Africa 	14,317,996 
+ South Africa 	Africa 	Southern Africa 	56,015,473 
+ South Korea 	Asia 	Eastern Asia 	50,791,919 
+ South Sudan 	Africa 	Eastern Africa 	12,230,730 
+ Spain[c] 	Europe 	Southern Europe 	46,347,576 
+ Sri Lanka 	Asia 	Southern Asia 	20,798,492 
+ Sudan 	Africa 	Northern Africa 	39,578,828 
+ Suriname 	Americas 	South America 	558,368 
+ Swaziland 	Africa 	Southern Africa 	1,343,098 
+ Sweden 	Europe 	Northern Europe 	9,837,533 
+  Switzerland 	Europe 	Western Europe 	8,401,739 
+ Syria 	Asia 	Western Asia 	18,430,453 
+ Taiwan[g] 	Asia 	Eastern Asia 	23,556,706 
+ Tajikistan 	Asia 	Central Asia 	8,734,951 
+ Tanzania[b] 	Africa 	Eastern Africa 	55,572,201 
+ Thailand 	Asia 	South-Eastern Asia 	68,863,514 
+ The Gambia 	Africa 	Western Africa 	2,038,501 
+ Togo 	Africa 	Western Africa 	7,606,374 
+ Tokelau 	Oceania 	Polynesia 	1,282 
+ Tonga 	Oceania 	Polynesia 	107,122 
+ Trinidad and Tobago 	Americas 	Caribbean 	1,364,962 
+ Tunisia 	Africa 	Northern Africa 	11,403,248 
+ Turkey 	Asia 	Western Asia 	79,512,426 
+ Turkmenistan 	Asia 	Central Asia 	5,662,544 
+ Turks and Caicos Islands 	Americas 	Caribbean 	34,900 
+ Tuvalu 	Oceania 	Polynesia 	11,097 
+ Uganda 	Africa 	Eastern Africa 	41,487,965 
+ Ukraine[d] 	Europe 	Eastern Europe 	44,438,625 
+ United Arab Emirates 	Asia 	Western Asia 	9,269,612 
+ United Kingdom 	Europe 	Northern Europe 	65,788,574 
+ United States 	Americas 	Northern America 	322,179,605 
+ United States Virgin Islands 	Americas 	Caribbean 	104,913 
+ Uruguay 	Americas 	South America 	3,444,006 
+ Uzbekistan 	Asia 	Central Asia 	31,446,795 
+ Vanuatu 	Oceania 	Melanesia 	270,402 
+  Vatican City[s] 	Europe 	Southern Europe 	801 
+ Venezuela 	Americas 	South America 	31,568,179 
+ Vietnam 	Asia 	South-Eastern Asia 	94,569,072 
+ Wallis and Futuna 	Oceania 	Polynesia 	11,899 
+ Western Sahara 	Africa 	Northern Africa 	538,755 
+World 	— 	— 	7,466,964,280 
+ Yemen 	Asia 	Western Asia 	27,584,213 
+ Zambia 	Africa 	Eastern Africa 	16,591,390 
+ Zimbabwe 	Africa 	Eastern Africa 	16,150,362 '''
--- day_03/day03.md
+++ day_03/day03.md
@@ -1,81 +1,197 @@
-Forritunarmálið Python
-======================
-
-Dagur 3
--------
-
-
+---
+title: Forritunarmálið Python
+subtitle: Day 3 \newline Modules and Functional Programming
+author: Hjalti Magnússon
+date: 29.\ nóvember 2017
 ---
 
-### String formatting
+# Modules
 
-* Old way
-* *New* way
-* Actual new way (format strings)
+## Python modules
+
+* Python Standard Library is a large library of very useful things
+* Syntax
+    * `import module (as alias)`
+    * `form module import fun/cls (as alias)`
+* *All* python files are modules
+
+## Example
+
+```
+>>> import string
+>>> dir(string)
+
+>>> from string import punctuation
+>>> punctuation
+```
+
+# Extending the basics
+
+## `decimal` - arbitrary precision floating point numbers
+
+```
+>>> from decimal import Decimal as D
+>>> D('1.2') + D('2.2')
+>>> 1.2 + 2.2
+
+>>> D('0.1') + D('0.1') + D('0.1') - D('0.3')
+>>> 0.1 + 0.1 + 0.1 - 0.3
+```
+
+## `fractions` - rational numbers
+
+```
+>>> from fractions import Fraction as Fr
+>>> Fr(2, 3) + Fr(1, 3)
+>>> Fr(2, 3) + Fr(1, 3) == 1
+>>> Fr(1.2)
+>>> Fr('1.2')
+>>> Fr('9/5')
+>>> f = Fr('66.6')
+>>> f.numerator
+>>> f.denominator
+```
+
+## `collections` - more data structures
+
+* `defaultdict`
+    * A nicer version of dictionary
+
+* `Counter`
+    * One of Python's coolest data structures
+
+* `deque`
+    * If you need fast prepend, append
+
+## `datetime` - date and time
+
+* A module that allows us to work with
+    * Dates
+    * Time
+    * Dates with time
+    * Time intervals
+
+## `datetime`
+
+```
+>>> from datetime import datetime, date
+>>> datetime.now()
+>>> date.today()
+
+>>> d1 = datetime(2017, 10, 28)
+>>> d2 = datetime.now()
+>>> d2 - d1
+```
+
+## Printing and parsing
+
+```
+>>> now = datetime.now()
+>>> now.strftime('%d-%m-%Y %H:%M:%S')
+>>> now.strftime('%d. %B %Y at %H:%M:%S')
+
+>>> now.strptime('29-09-2018', '%d-%m-%Y')
+
+>>> import time
+>>> time.time()
+>>> datetime.fromtimestamp(time.time())
+```
+
+# Debugging
+
+## `pdb` - the Python debugger
+
+Python comes with a built in debugger, pdb
+
+```
+# Set a breakpoint
+import pdb; pdb.set_trace()
+```
+
+## Commands
+
+* **s(tep)** "Jump into"
+* **n(next)** "Jump over"
+* **c(ontinue)** Run until next breakpoint
+* **r(eturn)** Run until current function returns
+* **l(ist)** (**ll**) Print where you are
+* **p(rint)** (**pp**) Print
+* **q(uit)** Quit the debugger
+* **!** Execute statement
+* **interact** Start Python interpreter
+
+# Functions
+
+## More about functions
+
+* In Python functions are first class citizens
+    * They can be passed as variables
+    * They can be returned by functions
 
 
----
+## Lambda functions
 
-### Functions
+Python provides anonymous functions
 
-* Nested functions
-* Anonymous functions
+```
+>>> lambda x: x ** 2
 
----
+>>> f = lambda x: x ** 2
+>>> f(4)
 
-### Functional programming
+# They can take more than one parameter
+>>> g = lambda x, y: x * y
+>>> g(4, 5)
+
+# Or none
+>>> h = lambda: 1377
+>>> h()
+```
+
+# Functional programming
+
+## Lists
+
+Many builtin functions in Python take functions as parameters
 
 * `min, max, sum`
 * `map, filter, all, any`
 * `sorted`
-* `functools`
 
----
+# Problems
 
-### Importing modules
+## Problem 0
 
-* `import mod (as alias)`
-* `form mod import fun/cls (as alias)`
-* *All* python files are modules
+Find the word count of The Raven
 
----
+## Problem 1
 
-### itertools
+Find the country with the most unique letters
 
-* `cycle`
-* `product`
-* `permutations`
-* `combinations`
-* `groupby`
+## Problem 2
 
----
+Find the country in Europe with the highest population
 
-### Assert
+## Problem 3
 
-* assert statement
-* two forms
+Sort all countries by population
 
----
+## Problem 4
 
-### Builtin numeric functions
+Sort all countries by population
 
-* `int, float, bin, oct, hex`
-* `abs, round`
+## Problem 5
 
-### Extra numeric types
+Find the user with the most accepted submissions
 
-* `fractions`
-* `decimal`
+## Problem 6
 
-### collections
+For each user that has submitted problems, find the number of solved problems he has solved an his full name
 
-* `defaultdict`
-* `Counter`
 
----
+# Assert
 
-### Meta
+## Assert is a friend
 
-* `eval`
-* `exec`
-* `compile`
+* Assert that the state of your program is consistent
+    * Check that "this should never happen" doesn't happen
